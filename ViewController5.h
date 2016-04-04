37e0e11bf7a41614e1e301e3cf5505eed1eaa6c8
xinyu <xinyu.yan1008@gmail.com>
2016-04-03 21:17:43 -0400

    动态动画－重力＋吸附

---
+++ ViewController5.h
@@ -0,0 +1,14 @@
+//
+//  ViewController5.h
+//  coreAnimaSY
+//
+//  Created by Xinyu Yan on 4/3/16.
+//  Copyright © 2016 Xinyu Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController5 : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ ViewController5.m
@@ -0,0 +1,89 @@
+//
+//  ViewController5.m
+//  coreAnimaSY
+//
+//  Created by Xinyu Yan on 4/3/16.
+//  Copyright © 2016 Xinyu Yan. All rights reserved.
+//
+
+/*
+1. 创建一个uidynamicanimator，大部分情况下只需要一个animator；
+2. 创建一个或多个uidynamic behavior, 并添加到animator;
+3. 为dynamic behavior 添加 dynamic item
+*/
+
+
+#import "ViewController5.h"
+
+@interface ViewController5 ()
+
+@property (strong,nonatomic) UIDynamicAnimator *animator;
+
+@end
+
+@implementation ViewController5
+
+@synthesize imageView;
+
+-(UIDynamicAnimator *)animator{
+    if (!_animator) {
+        _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    }
+    return _animator;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)reset{
+    [self.animator removeAllBehaviors];
+    imageView.center = CGPointMake(80, 106);
+}
+
+- (IBAction)back:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)snap:(id)sender {
+}
+
+- (IBAction)Collision:(id)sender {
+}
+
+- (IBAction)gravity:(id)sender {
+    [self reset];
+    UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] init];
+//    gravityBehavior.angle = M_PI_2;  //重力的方向（由弧度指定）
+    gravityBehavior.gravityDirection = CGVectorMake(0, 1);  //重力的方向（由vector指定）
+    gravityBehavior.magnitude = 0.5;   //重力加速度
+    [gravityBehavior addItem:imageView];
+    [self.animator addBehavior:gravityBehavior];
+    
+}
+- (IBAction)attach:(id)sender {
+    [self reset];
+    UIAttachmentBehavior *attachBeahavior =[[UIAttachmentBehavior alloc] initWithItem:imageView attachedToAnchor:CGPointMake(CGRectGetMidX(self.view.frame), 106)];
+    UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[imageView]];
+    [self.animator addBehavior:attachBeahavior];
+    [self.animator addBehavior:gravityBehavior];
+}
+- (IBAction)push:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
